424cc8035b89941f603b8d203188a04a2d3ba152
jontaylor613-hub <jontaylor613@gmail.com>
2025-12-03 21:34:31 -0500

    Create generate.js

---
+++ prism-path/api/generate.js
@@ -0,0 +1,6 @@
+// In /api/generate.js
+// Change this line:
+const apiKey = process.env.VITE_GEMINI_API_KEY; 
+
+// To this (referencing the new secret name):
+const apiKey = process.env.GEMINI_API_KEY;
